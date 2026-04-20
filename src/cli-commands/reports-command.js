b396858bdb38edd6d78cc8bb06367deffaf7c302
Tis5813 <pavel.tisner@gmail.com>
2026-04-20 22:40:12 +0200

    Added deleteMeal, listofMeal, currentReport

--- src/cli-commands/reports-command.js
+++ src/cli-commands/reports-command.js
@@ -1,14 +1,25 @@
 import { generateReport } from '../intergrations/ai.js';
-import { getTodaysMealsByUserId } from '../repositories/meal-repository.js';
+import {
+  getTodaysMealsByUserId,
+  getDailyTotals,
+} from '../repositories/meal-repository.js';
 import { getUserGoals } from '../repositories/goal-repository.js';
+import { listofMeal } from '../services/meal-service.js';
 
 import chalk from 'chalk';
 
-export async function reportCli(subcommand, args, currentUser) {
-  if (subcommand === 'SHOW') {
-    await dailyReports(currentUser.id);
-  } else if (!subcommand === 'SHOW') {
-    console.log(chalk.red('Invalid Report Command. Try: REPORT SHOW'));
+export async function reportCli(subcommand, _, currentUser) {
+  switch (subcommand) {
+    case 'SHOW':
+      await dailyReports(currentUser.id);
+      break;
+    case 'CURRENT':
+      await currentReport(currentUser.id);
+      break;
+    default:
+      console.log(
+        chalk.red('Invalid Report Command. Try: REPORT SHOW or REPORT CURRENT')
+      );
   }
 }
 
@@ -36,4 +47,31 @@ export async function dailyReports(userId) {
   const goals = getUserGoals(userId);
   const report = await generateReport(goals, meals);
   console.log(report);
+}
+
+export async function currentReport(userId) {
+  listofMeal(userId);
+  const {
+    total_kcal,
+    total_protein,
+    total_fat,
+    total_carbs,
+    total_water,
+    total_caffeine,
+    total_alcohol,
+  } = getDailyTotals(userId)[0];
+  
+  console.log('-'.repeat(95));
+
+  console.log(
+    "Totals".padEnd(10) + ' | ' +
+      ` `.padEnd(18) + ' | ' +
+      total_kcal.toString().padEnd(5) + ' | ' +
+      `${total_protein}g`.padEnd(5) + ' | ' +
+      `${total_fat}g`.padEnd(5) + ' | ' +
+      `${total_carbs}g`.padEnd(8) + ' | ' +
+      `${total_water}ml`.padEnd(5) + ' | ' +
+      `${total_caffeine}mg`.padEnd(8) + ' | ' +
+      `${total_alcohol}g`
+    );
 }
